b9d8a7b7491ce7895176a308136283fc4f071a4d
yousra <sakeryousra719@gmail.com>
2026-04-11 22:47:22 +0200

    Update project files and database

--- statues/urls.py
+++ statues/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StatueViewSet
-from .views import chatbot
+from .views import StatueViewSet, chatbot
 
 router = DefaultRouter()
 router.register(r'statues', StatueViewSet)
